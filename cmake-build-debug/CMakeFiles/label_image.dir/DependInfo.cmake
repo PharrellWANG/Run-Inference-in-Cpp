fd851a8631d64f1e02ca20634e8de17c7cc52f01
Pharrell.zx <wzxnuaa@gmail.com>
2017-09-14 17:18:50 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/label_image.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Pharrell_WANG/ClionProjects/tf_cpp/main.cpp" "/Users/Pharrell_WANG/ClionProjects/tf_cpp/cmake-build-debug/CMakeFiles/label_image.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/tensorflow"
+  "/usr/local/include/tensorflow/bazel-genfiles"
+  "/usr/local/include/tensorflow/tensorflow/contrib/makefile/downloads"
+  "/usr/local/include/tensorflow/tensorflow/contrib/makefile/downloads/eigen"
+  "/usr/local/include/tensorflow/tensorflow/contrib/makefile/downloads/gemmlowp"
+  "/usr/local/include/tensorflow/tensorflow/contrib/makefile/gen/protobuf-host/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/label_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/label_image.dir/main.cpp.o"
+  "label_image.pdb"
+  "label_image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/label_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
